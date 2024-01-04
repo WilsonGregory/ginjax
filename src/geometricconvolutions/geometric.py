32891964eadba44eae90adb3226e70405846ad4a
Wilson Gregory <wgregor4@jhu.edu>
2024-01-04 16:37:15 -0500

    revert make_all_operators to returning a list

--- src/geometricconvolutions/geometric.py
+++ src/geometricconvolutions/geometric.py
@@ -58,8 +58,7 @@ def make_all_operators(D):
     possible_entries = [np.diag(prod) for prod in it.product([1,-1], repeat=D)]
 
     #combine all the permutation matrices with the possible entries, then flatten to a single array of operators
-    operator_list = list(it.chain(*list(map(lambda matrix: [matrix @ prod for prod in possible_entries], permutation_matrices))))
-    return jnp.stack(operator_list)
+    return list(it.chain(*list(map(lambda matrix: [matrix @ prod for prod in possible_entries], permutation_matrices))))
 
 # ------------------------------------------------------------------------------
 # PART 2: Define the Kronecker Delta and Levi Civita symbols to be used in Levi Civita contractions

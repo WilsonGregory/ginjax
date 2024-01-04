7155ef49dea44a1070e94f6f9721db7d3375fab4
Wilson Gregory <wgregor4@jhu.edu>
2024-01-04 16:28:20 -0500

    cleanup equiv_test script

--- scripts/phase2vec/equiv_test.py
+++ scripts/phase2vec/equiv_test.py
@@ -21,7 +21,6 @@ conv_filters = geom.get_invariant_filters(Ms=[3], ks=[0,1,2,3], parities=[0,1], 
 
 # generate function library
 ode_basis = p2v_models.get_ode_basis(D, N, [-1.,-1.], [1.,1.], 3) # (N**D, 10)
-print(ode_basis.shape)
 num_coeffs = ode_basis.shape[1]
 small_ode_basis = p2v_models.get_ode_basis(D, 5, [-1.,-1.], [1.,1.], 3)
 operators_on_coeffs = geom.get_operators_on_coeffs(D, operators, small_ode_basis)
@@ -30,7 +29,6 @@ key, subkey = random.split(key)
 rand_coeffs = random.normal(subkey, shape=(num_coeffs, D))
 
 vec_img = (ode_basis @ rand_coeffs).reshape((N,)*D + (D,))
-print(vec_img.shape)
 
 for gg, gg_coeffs in zip(operators, operators_on_coeffs):
     rotated_coeffs = (gg_coeffs @ rand_coeffs.reshape((num_coeffs*D,))).reshape((num_coeffs,D))
@@ -42,23 +40,13 @@ for gg, gg_coeffs in zip(operators, operators_on_coeffs):
 # an equivariant process. That is the following are equivalent:
 # generate coeffs -> make image -> rotate image
 # take generated coeffs -> rotate coeffs -> make image
+print('All rotation/reflection tests passed!')
+
 # Now we want to do the same for translations
 
 n = 2
 N = 2*n + 1
 large_small_ode_basis = jnp.round(p2v_models.get_ode_basis(D, N*3, [-(n+N), -(n+N)], [n+N, n+N], 3))
-# print(large_small_ode_basis.reshape((3*N,)*D + (num_coeffs,))[:,:,1])
-# print(large_small_ode_basis.reshape((3*N,)*D + (num_coeffs,))[:,:,1].shape)
-
-
-# for i, e in enumerate(jnp.eye(num_coeffs * D)):
-#     print(i)
-#     shaped_e = e.reshape((num_coeffs,D))
-#     print(shaped_e)
-#     vec_img = (small_ode_basis @ shaped_e).reshape((5,)*D + (D,))
-#     print(vec_img)
-#     print(vec_img[1,0])
-#     print(vec_img[0,1])
 
 """
 shape when (10,2)
@@ -96,18 +84,6 @@ def get_translation_op(a,b):
 
 key, subkey = random.split(key)
 rand_coeffs = random.normal(subkey, shape=(num_coeffs,D))
-# rand_coeffs = jnp.array([
-#     [1, 0],
-#     [0, 0],
-#     [0, 0],
-#     [0, 1],
-#     [0, 0],
-#     [-1, 0],
-#     [0, 0],
-#     [0, 0],
-#     [0, 0],
-#     [0, 0],
-# ])
 
 for (a,b) in it.product(range(-N,N+1),range(-N,N+1)):
     translation_gg = get_translation_op(a,b)
@@ -118,8 +94,4 @@ for (a,b) in it.product(range(-N,N+1),range(-N,N+1)):
 
     assert jnp.allclose(first, second, atol=geom.TINY, rtol=geom.TINY), f'{a},{b}: {jnp.max(first - second)}'
 
-
-
-# can do 16
-# invariant_gen_filters = geom.get_invariant_gen_filters(16, 3, D, 2, 0, operators)
-# print(invariant_gen_filters.shape)
+print('All translation tests passed!')
